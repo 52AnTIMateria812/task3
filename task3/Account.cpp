ab46e5305060d8a894a14646a90d519ad9add5d6
52AnTIMateria812 <sysoevtim19@gmail.com>
2025-03-26 12:33:21 +0300

    Update Account.cpp

--- task3/Account.cpp
+++ task3/Account.cpp
@@ -1,8 +1,8 @@
 #include "Account.h"
 
 Account::Account(const std::string& accId, std::shared_ptr<Client> accOwner, 
-               std::shared_ptr<Bank> accBank, AccountType accType)
-    : id(accId), balance(0.0), owner(accOwner), bank(accBank), type(accType) {}
+               std::shared_ptr<Bank> accBank)
+    : id(accId), balance(0.0), owner(accOwner), bank(accBank) {}
 
 void Account::deposit(double amount) {
     if (amount <= 0) throw std::invalid_argument("Amount must be positive");
@@ -16,7 +16,6 @@ void Account::transfer(std::shared_ptr<Account> toAccount, double amount) {
 
 double Account::getBalance() const { return balance; }
 std::string Account::getId() const { return id; }
-AccountType Account::getType() const { return type; }
 std::shared_ptr<Client> Account::getOwner() const { return owner; }
 std::shared_ptr<Bank> Account::getBank() const { return bank; }
 
