5ad9211edbb561d915261852fc72ff1f5a42de59
52AnTIMateria812 <sysoevtim19@gmail.com>
2025-03-26 10:23:08 +0300

    DebitAccount.cpp

---
+++ task3/DebitAccount.cpp
@@ -0,0 +1,15 @@
+#include "DebitAccount.h"
+
+DebitAccount::DebitAccount(const std::string& accId, std::shared_ptr<Client> accOwner, 
+                         std::shared_ptr<Bank> accBank, double rate)
+    : Account(accId, accOwner, accBank, AccountType::DEBIT), interestRate(rate) {}
+
+void DebitAccount::withdraw(double amount) {
+    if (amount <= 0) throw std::invalid_argument("Amount must be positive");
+    if (balance < amount) throw std::runtime_error("Insufficient funds");
+    balance -= amount;
+}
+
+void DebitAccount::applyInterest() {
+    balance += balance * (interestRate / 100 / 365);
+}
